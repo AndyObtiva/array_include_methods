a29dfdb4925484c5019052532d9ac617d53c65a1
Andy Maleh <andy.am@gmail.com>
2020-06-01 18:27:12 -0400

    A more performant implementation of Array#include_all? when comparing arrays with different object classes

--- lib/array_include_methods.rb
+++ lib/array_include_methods.rb
@@ -6,11 +6,18 @@ module ArrayIncludeMethods
     # Always returns `false` if the given `array` is nil
     def include_all?(array)
       return false if array.nil?
-      if map(&:class).uniq.size == 1 && array.map(&:class).uniq.size == 1
-        (self & array).uniq.sort == array.uniq.sort
-      else
-        array.reduce(true) { |result, element| result && include?(element) }
-      end    
+      array_include_other_array_same_class_elements = lambda do |a1, a2|
+        (a1 & a2).uniq.sort == a2.uniq.sort
+      end
+      self_grouped_by = self.group_by(&:class)
+      array_grouped_by = array.group_by(&:class)
+      return false unless array_include_other_array_same_class_elements.call(self_grouped_by.keys.map(&:to_s), array_grouped_by.keys.map(&:to_s))
+      array_grouped_by.reduce(true) do |result, pair|
+        array_class = pair.first
+        array_elements = pair.last
+        self_grouped_by[array_class]
+        result && array_include_other_array_same_class_elements.call(self_grouped_by[array_class], array_elements)
+      end
     end
 
     # Returns `true` if any of the given `array` elements are present in `self`,
